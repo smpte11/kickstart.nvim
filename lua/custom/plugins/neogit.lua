f2060615af83cb285dc6759baa40774b91758051
Felix Brousseau-Tremblay <f.brousseau-tremblay@klarna.com>
2024-11-12 14:13:02 +0100

    First commit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      -- 'ibhagwan/fzf-lua', -- optional
+    },
+    keys = {
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Git Status (Neogit)' },
+    },
+    config = true,
+  },
+}
