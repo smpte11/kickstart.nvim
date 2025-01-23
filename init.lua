534af3b1794e03faee19ddd59379fc05a6b4a207
Felix Brousseau-Tremblay <f.brousseau-tremblay@klarna.com>
2025-01-23 15:08:00 +0100

    Use mini notify

--- init.lua
+++ init.lua
@@ -562,9 +562,6 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', opts = {} },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
-
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
     },
@@ -1062,6 +1059,15 @@ require('lazy').setup({
       require('mini.tabline').setup {}
 
       require('mini.sessions').setup {}
+
+      -- notification widget
+      local win_config = function()
+        local has_statusline = vim.o.laststatus > 0
+        local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+        return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
+      end
+
+      require('mini.notify').setup { window = { config = win_config } }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
